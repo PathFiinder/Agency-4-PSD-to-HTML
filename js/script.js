b766896456a422960d9743b17b92caa6b4f079fa
Krystian Konecki <krystian_konecki@wp.pl>
2019-07-09 14:51:47 +0200

    Adding clients section

--- js/script.js
+++ js/script.js
@@ -5,6 +5,21 @@ const headerMenu = document.querySelector('.header__menu');
 sloganButton.addEventListener('click', () => {
     header.classList.toggle('header--active');
     headerMenu.classList.toggle('header__menu--active');
-   
+})
+
+
+
+
+//Clients
+const clientsMiniatures = document.querySelectorAll('.miniature-images__photo');
+const clientsArray = document.querySelectorAll('.clients__single-item');
+
+[...clientsMiniatures].forEach((element) => {
+    element.addEventListener('click', (event) => {
+        [...clientsMiniatures].forEach(element => element.classList.remove('miniature-images__photo--active'))
+        element.classList.add('miniature-images__photo--active');
+        [...clientsArray].forEach(element => element.style.display = "none")
+        document.querySelector(`[data-id = "${event.target.dataset.idm}"]`).style.display = "block";
+    })
 
 })
