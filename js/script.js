f5b9f2de2f8f4630cd0d343466728e3f41ae204e
Krystian Konecki <krystian_konecki@wp.pl>
2019-07-10 16:32:41 +0200

    Adding Rwd version 1

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+//Navbar
 const sloganButton = document.querySelector('.fa-bars');
 const header = document.querySelector('.header');
 const headerMenu = document.querySelector('.header__menu');
@@ -6,6 +7,44 @@ sloganButton.addEventListener('click', () => {
     header.classList.toggle('header--active');
     headerMenu.classList.toggle('header__menu--active');
 })
+
+//Scroll
+const sloganPosTop = $('.slogan').offset().top;
+const aboutPosTop = $('.about-company').offset().top
+const servicesPosTop = $('.company-services').offset().top
+const workPosTop = $('.company-work').offset().top
+const teamPosTop = $('.team').offset().top
+const clientsPosTop = $('.clients').offset().top
+const contactPosTop = $('.contact').offset().top
+
+const buttonsArray = ['home','about-us','services','work','team','clients','process'];
+const posTopArray = [sloganPosTop,aboutPosTop,servicesPosTop,workPosTop,teamPosTop,clientsPosTop,contactPosTop]
+
+function toScroll(PosTop) {
+    $('body, html').animate({
+        scrollTop: PosTop
+    }, 1000)
+    header.classList.remove("header--active")
+    headerMenu.classList.remove("header__menu--active")
+}
+
+buttonsArray.forEach((element,index) => {
+    document.querySelector(`.header__link--${element}`).addEventListener('click', () => {
+        toScroll(posTopArray[index]);
+    })
+});
+
+
+
+
+
+
+
+
+
+
+
+
 
 //Team
 
